e2fdf41dc6e9ae087e35f2f0fb2f9b133f5d28b9
Warrick Moran <warrick.moran@noaa.gov>
2018-11-19 19:20:23 +0000

    Initial MapBox

---
+++ src/main/java/inotifyem/MapboxController.java
@@ -0,0 +1,17 @@
+package inotifyem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MapboxController{
+
+    @GetMapping("/mapbox")
+    public String mapbox(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "mapbox";
+    }
+
+}
